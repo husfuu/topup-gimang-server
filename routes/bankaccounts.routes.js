c61517e6a9ec5192e1382922359e705111506c63
husfuu <muhammadhusninurfadillah@gmail.com>
2022-07-26 23:33:06 +0800

    Categories CRUD API

--- routes/bankaccounts.routes.js
+++ routes/bankaccounts.routes.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bankAccount = require('../controllers/bankaccounts.controllers');
+const router = require("express").Router();
+const bankAccount = require("../controllers/bankaccounts.controllers");
 
-router.post('/banks', bankAccount.createBankAccount);
-router.get('/banks/:id', bankAccount.getBankAccountById);
-router.get('/banks', bankAccount.getAllBankAccounts);
-router.put('/banks/:id', bankAccount.updateBankAccountById);
-router.delete('/banks/:id', bankAccount.deleteBankAccountById);
+router.post("/banks", bankAccount.createBankAccount);
+router.get("/banks/:id", bankAccount.getBankAccountById);
+router.get("/banks", bankAccount.getAllBankAccounts);
+router.put("/banks/:id", bankAccount.updateBankAccountById);
+router.delete("/banks/:id", bankAccount.deleteBankAccountById);
 
 module.exports = router;
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const categoryController = require('../controllers/categories.controllers');
 
-router.get('/category', categoryController.viewCategories);
+router.post("/categories", categoryController.createCategory);
+router.get("/categories", categoryController.getAllCategories);
+router.get("/categories/:id", categoryController.getCategoryById);
+router.put("/categories/:id", categoryController.updateCategoryById);
+router.delete("/categories/:id", categoryController.deleteCategoryById);
 
 module.exports = router;
