7088183f9cab8de80546ad09346512e49bc57403
husfuu <muhammadhusninurfadillah@gmail.com>
2022-07-31 16:51:51 +0800

    Category fiture

--- controllers/categories.controllers.js
+++ controllers/categories.controllers.js
@@ -1,209 +1,312 @@
 const { Categories } = require("../models");
 
 exports.createCategory = async (req, res) => {
-  try {
-    const { name } = req.body;
+    try {
+        const { name } = req.body;
 
-    if (!name) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: "please fill the category name",
-        },
-      });
+        if (!name) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "please fill the category name",
+                },
+            });
+        }
+
+        const categoryExists = await Categories.findOne({
+            where: {
+                name,
+            },
+        });
+
+        if (categoryExists) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "category is already exists!",
+                },
+            });
+        }
+
+        const category = await Categories.create({
+            name,
+        });
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                message: "New category successfully created!",
+                category,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
     }
-
-    const categoryExists = await Categories.findOne({
-      where: {
-        name,
-      },
-    });
-
-    if (categoryExists) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: "category is already exists!",
-        },
-      });
-    }
-
-    const category = await Categories.create({
-      name,
-    });
-
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        message: "New category successfully created!",
-        category,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "FAILED",
-      data: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-    });
-  }
 };
 
 exports.getAllCategories = async (req, res) => {
-  try {
-    const categories = await Categories.findAll();
+    try {
+        const categories = await Categories.findAll();
 
-    if (!categories) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: "the category not found!",
-        },
-      });
+        if (!categories) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "the category not found!",
+                },
+            });
+        }
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                message: "Successfully get all categories",
+                categories,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
     }
-
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        message: "Successfully get all categories",
-        categories,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "FAILED",
-      data: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-    });
-  }
 };
 
 exports.getCategoryById = async (req, res) => {
-  try {
-    const categoryId = req.params.id;
-    const category = await Categories.findByPk(categoryId);
+    try {
+        const categoryId = req.params.id;
+        const category = await Categories.findByPk(categoryId);
 
-    if (!category) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: `category with id = ${categoryId} is not found`,
-        },
-      });
+        if (!category) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: `category with id = ${categoryId} is not found`,
+                },
+            });
+        }
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                message: "Successfully get bank account!",
+                category,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
     }
-
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        message: "Successfully get bank account!",
-        category,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "FAILED",
-      data: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-    });
-  }
 };
 
 exports.updateCategoryById = async (req, res) => {
-  try {
-    const categoryId = req.params.id;
-    const category = await Categories.findByPk(categoryId);
+    try {
+        const categoryId = req.params.id;
+        const category = await Categories.findByPk(categoryId);
 
-    if (!category) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: `category with id = ${categoryId} is not found!`,
-        },
-      });
+        if (!category) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: `category with id = ${categoryId} is not found!`,
+                },
+            });
+        }
+
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "please fill the name",
+                },
+            });
+        }
+
+        await Categories.update(
+            {
+                name,
+            },
+            {
+                where: {
+                    id: categoryId,
+                },
+            },
+        );
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                message: `Successfully update category with id = ${categoryId}!`,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
     }
-
-    const { name } = req.body;
-
-    if (!name) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: "please fill the name",
-        },
-      });
-    }
-
-    await Categories.update(
-      {
-        name,
-      },
-      {
-        where: {
-          id: categoryId,
-        },
-      },
-    );
-
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        message: `Successfully update category with id = ${categoryId}!`,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "FAILED",
-      data: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-    });
-  }
 };
 
 exports.deleteCategoryById = async (req, res) => {
-  try {
-    const categoryId = req.params.id;
-    const category = await Categories.findByPk(categoryId);
+    try {
+        const categoryId = req.params.id;
+        const category = await Categories.findByPk(categoryId);
 
-    if (!category) {
-      return res.status(401).json({
-        status: "FAILED",
-        data: {
-          message: `category with id = ${categoryId} is not found!`,
-        },
-      });
+        if (!category) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: `category with id = ${categoryId} is not found!`,
+                },
+            });
+        }
+
+        await Categories.destroy({
+            where: {
+                id: categoryId,
+            },
+        });
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                message: `Successfully delete category with id = ${categoryId}`,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
     }
+};
 
-    await Categories.destroy({
-      where: {
-        id: categoryId,
-      },
-    });
+exports.viewAllCategories = async (req, res) => {
+    try {
+        const categories = await Categories.findAll();
 
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        message: `Successfully delete category with id = ${categoryId}`,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "FAILED",
-      data: {
-        name: error.name,
-        message: error.message,
-        stack: error.stack,
-      },
-    });
-  }
+        res.render("admin/category/view_category", {
+            title: "Category Page",
+            categories,
+        });
+    } catch (error) {
+        res.redirect("/category");
+    }
+};
+
+exports.viewCreateCategories = async (req, res) => {
+    try {
+        res.render("admin/category/add_category", {
+            title: "Add Category",
+        });
+    } catch (error) {
+        res.redirect("/categories");
+    }
+};
+
+exports.viewEditCategories = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const category = await Categories.findByPk(categoryId);
+
+        res.render("admin/category/edit_category", {
+            category,
+            title: "Edit Category",
+        });
+    } catch (error) {
+        res.redirect("/category");
+    }
+};
+
+exports.actionCreateCategories = async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "please fill the category name",
+                },
+            });
+        }
+
+        const category = await Categories.create({
+            name,
+        });
+
+        res.redirect("/categories");
+    } catch (error) {}
+};
+
+exports.actionEditCategories = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+
+        const { name } = req.body;
+
+        await Categories.update(
+            {
+                name,
+            },
+            {
+                where: {
+                    id: categoryId,
+                },
+            },
+        );
+
+        res.redirect("/categories");
+    } catch (error) {}
+};
+
+exports.actionDeleteCategories = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        // const category = await Categories.findByPk(categoryId);
+
+        await Categories.destroy({
+            where: {
+                id: categoryId,
+            },
+        });
+
+        res.redirect("/categories");
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
+    }
 };
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,10 +1,23 @@
-const router = require('express').Router();
-const categoryController = require('../controllers/categories.controllers');
+const router = require("express").Router();
+const categoryController = require("../controllers/categories.controllers");
 
-router.post("/categories", categoryController.createCategory);
-router.get("/categories", categoryController.getAllCategories);
-router.get("/categories/:id", categoryController.getCategoryById);
-router.put("/categories/:id", categoryController.updateCategoryById);
-router.delete("/categories/:id", categoryController.deleteCategoryById);
+router.post("/api/v1/categories", categoryController.createCategory);
+router.get("/api/v1/categories", categoryController.getAllCategories);
+router.get("/api/v1/categories/:id", categoryController.getCategoryById);
+router.put("/api/v1/categories/:id", categoryController.updateCategoryById);
+router.delete("/api/v1/categories/:id", categoryController.deleteCategoryById);
+
+// view
+router.get("/categories", categoryController.viewAllCategories);
+router.get("/categories/create", categoryController.viewCreateCategories);
+router.get("/categories/edit/:id", categoryController.viewEditCategories);
+
+// actions
+router.post("/categories/create", categoryController.actionCreateCategories);
+router.post(
+    "/categories/delete/:id",
+    categoryController.actionDeleteCategories,
+);
+router.post("/categories/edit/:id", categoryController.actionEditCategories);
 
 module.exports = router;
