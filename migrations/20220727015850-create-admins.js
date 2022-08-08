9e9d91e87988a24c5e9211d09b15ff1807c8633b
husfuu <muhammadhusninurfadillah@gmail.com>
2022-08-08 12:30:07 +0800

    changes in Admins Table -- add fullName as a column in the Admins Table

--- migrations/20220727015850-create-admins.js
+++ migrations/20220727015850-create-admins.js
@@ -1,30 +1,34 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Admins', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Admins');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Admins", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            fullName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+            },
+            password: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Admins");
+    },
 };
--- models/admins.js
+++ models/admins.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     }
     Admins.init(
         {
+            fullName: DataTypes.STRING,
             email: DataTypes.STRING,
             password: DataTypes.STRING,
         },
