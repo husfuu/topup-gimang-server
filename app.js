3bad75293b16ca7c259f21acdd4dd649c32db7e9
husfuu <muhammadhusninurfadillah@gmail.com>
2022-08-18 17:53:52 +0800

    install dotenv and change database config to using env file

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const flash = require("connect-flash");
+const dotenv = require("dotenv");
 
+dotenv.config();
 // ROUTES
 // const indexRouter = require('./routes/index');
 const admin = require("./routes/admins.routes");
@@ -18,6 +20,7 @@ const roleRoutes = require("./routes/roles.routes");
 const transactionRoutes = require("./routes/transactions.routes");
 const userbiodataRoutes = require("./routes/userbiodatas.routes");
 const voucherRoutes = require("./routes/vouchers.routes");
+const checkoutRoutes = require("./routes/checkout.routes");
 
 const app = express();
 
@@ -56,6 +59,7 @@ app.use(paymentRoutes);
 app.use(transactionRoutes);
 app.use(userbiodataRoutes);
 app.use(voucherRoutes);
+app.use(checkoutRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
