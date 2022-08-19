b76784e80362df660c19475f65d764c8ed553c85
husfuu <muhammadhusninurfadillah@gmail.com>
2022-08-19 17:00:39 +0800

    test: change to  false

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
     session({
         secret: "keyboard cat",
         resave: false,
-        saveUninitialized: true,
+        saveUninitialized: false,
         cookie: {
             secure: true,
             maxAge: 60000,
