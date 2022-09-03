0f07596ec7c3bc9f021dc2dc8846267eadc0dd67
M. Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2022-09-03 22:44:07 +0800

    add spent-transactions, latest-transactions, and detail-transaction features

---
+++ controllers/player.controllers.js
@@ -0,0 +1,137 @@
+const {
+    Transactions,
+    Nominals,
+    Vouchers,
+    BankAccounts,
+    Categories,
+    sequelize,
+} = require("../models");
+
+exports.getSpentTransactions = async (req, res) => {
+    try {
+        const transactions = await Transactions.findAll({
+            where: { userId: req.player.id },
+            attributes: [
+                [sequelize.fn("sum", sequelize.col("value")), "total"],
+            ],
+            group: ["categoryId"],
+        });
+
+        if (!transactions) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "Transactions is not found!",
+                },
+            });
+        }
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                transactions,
+                message: "Succesfully get total spent each category",
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
+    }
+};
+
+exports.getLatestTransactions = async (req, res) => {
+    try {
+        const { status = "" } = req.query;
+
+        if (status !== "") {
+            const transactions = await Transactions.findAll({
+                where: { userId: req.player.id, status },
+                include: [Nominals],
+                order: [["createdAt", "DESC"]],
+            });
+
+            if (!transactions) {
+                return res.status(401).json({
+                    status: "FAILED",
+                    data: {
+                        message: "Transactions is not found!",
+                    },
+                });
+            }
+
+            res.status(201).json({
+                status: "SUCCESS",
+                data: {
+                    transactions,
+                    message: "Succesfully get all transaction by status",
+                },
+            });
+        } else {
+            const transactions = await Transactions.findAll({
+                where: { userId: req.player.id },
+                include: [Nominals],
+                order: [["createdAt", "DESC"]],
+            });
+
+            if (!transactions) {
+                return res.status(401).json({
+                    status: "FAILED",
+                    data: {
+                        message: "Transactions is not found!",
+                    },
+                });
+            }
+
+            res.status(201).json({
+                status: "SUCCESS",
+                data: {
+                    transactions,
+                    message: "Succesfully get all transaction by status",
+                },
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: "FAILED",
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack,
+            },
+        });
+    }
+};
+
+exports.getTransactionDetails = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const transaction = await Transactions.findOne({
+            where: { id },
+            include: [Vouchers, Nominals, BankAccounts, Categories],
+        });
+
+        if (!transaction) {
+            return res.status(401).json({
+                status: "FAILED",
+                data: {
+                    message: "Transactions is not found!",
+                },
+            });
+        }
+
+        res.status(201).json({
+            status: "SUCCESS",
+            data: {
+                transaction,
+                message: "Succesfully get all transaction by status",
+            },
+        });
+    } catch (error) {}
+};
---
+++ routes/players.routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const playerControllers = require("../controllers/player.controllers");
+const { isLoginPlayer } = require("../middlewares/auth");
+
+router.get(
+    "/api/v1/transaction-spent",
+    isLoginPlayer,
+    playerControllers.getSpentTransactions,
+);
+
+router.get(
+    "/api/v1/latest-transactions",
+    isLoginPlayer,
+    playerControllers.getLatestTransactions,
+);
+
+router.get(
+    "/api/v1/transactions/:id",
+    isLoginPlayer,
+    playerControllers.getTransactionDetails,
+);
+
+module.exports = router;
